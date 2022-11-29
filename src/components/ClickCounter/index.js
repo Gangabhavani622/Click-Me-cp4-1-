6cd3cb182b03e187531a293b719e959b2edaa58c
Gangabhavani622 <gangabhavani0628@gmail.com>
2022-11-29 09:30:26 +0000

    CP4(1) ClickMe Counter App

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,34 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class ClickCounter extends Component {
+  state = {count: 0}
+
+  clickMe = () => {
+    this.setState(prevState => {
+      return {count: prevState.count + 1}
+    })
+  }
+
+  render() {
+    const {count} = this.state
+
+    return (
+      <div className="container">
+        <div>
+          <h1 className="heading">
+            The Button has been clicked <span className="count">{count}</span>{' '}
+            times
+          </h1>
+          <p>Click the button to increase the count!</p>
+          <button className="button" onClick={this.clickMe}>
+            Click Me!
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
